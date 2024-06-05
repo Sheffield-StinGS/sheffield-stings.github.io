2e3c2fa401ae76ba04954367658c0f220a7d851e
Oscar <oscarlodgelego@gmail.com>
2024-06-05 11:06:21 +0100

    Removed Need for two submit form scripts

--- assets/scripts/submit_contact_form.js
+++ assets/scripts/submit_contact_form.js
@@ -3,58 +3,59 @@
 
 const form = document.querySelector("#form")
 const submitButton = document.querySelector("#submit")
-const scriptURL = 'https://script.google.com/macros/s/AKfycbwF2Lz3OEsQQuCQjZWXChTZaRvBjbvCMdQshgL4AOtxUuA2syjDxiyhNzEqAFgYCt4/exec'
+const scriptURL = '{{site.form_url}}'
 
-form["mode"].value = "dev"
+form["mode"].value = '{{site.form_mode}}'
 
 const errored_fields = []
 
-function validateForm (){       
+function validateForm (){      
+
     var first_name = form["first_name"];
     var email = form["email"];
     var email_pattern = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g;
     var purpose = form["purpose"];
     var regular_volunteering = form["regular"];
     var occasional_volunteering = form["occasional"];
-    var volunteering_frequency = form["volunteering_frequency"]
-    var warning_message = ["There were some problems with the data you entered:"]
+    var volunteering_frequency = form["volunteering_frequency"];
+    var warning_message = ["There were some problems with the data you entered:"];
 
     for (field of errored_fields){
-        $(field).removeClass("response_error")
+        $(field).removeClass("response_error");
     }
 
     if (first_name.value == ""){
-        warning_message.push("- First Name is missing")
-        $(first_name).addClass("response_error")
-        errored_fields.push(first_name)
+        warning_message.push("- First Name is missing");
+        $(first_name).addClass("response_error");
+        errored_fields.push(first_name);
     }
 
     if (email.value == ""){
-        warning_message.push("- Email is missing")
-        $(email).addClass("response_error")
-        errored_fields.push(email)
+        warning_message.push("- Email is missing");
+        $(email).addClass("response_error");
+        errored_fields.push(email);
     } else if (!(email_pattern.test(email.value))){
-        warning_message.push("- Email address not recognized")
-        $(email).addClass("response_error")
-        errored_fields.push(email)
+        warning_message.push("- Email address not recognized");
+        $(email).addClass("response_error");
+        errored_fields.push(email);
     }
 
     if (purpose.value == ""){
-        warning_message.push("- No reason for contact")
-        $(purpose).addClass("response_error")
-        errored_fields.push(purpose)
+        warning_message.push("- No reason for contact");
+        $(purpose).addClass("response_error");
+        errored_fields.push(purpose);
     } else if (purpose.value == "Volunteering"){
         if (!(regular_volunteering.checked || occasional_volunteering.checked)){
-            warning_message.push("- Please select a type of volunteering")
-            $(volunteering_frequency).addClass("response_error")
-            errored_fields.push(volunteering_frequency)
+            warning_message.push("- Please select a type of volunteering");
+            $(volunteering_frequency).addClass("response_error");
+            errored_fields.push(volunteering_frequency);
         }
     }
 
     if (warning_message.length == 1){
         return false
     } else{
-        alert(warning_message.join("\n"))
+        alert(warning_message.join("\n"));
         return true
     }
 }
